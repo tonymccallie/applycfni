b72286f61e7e5ddfd4dee7c17d20e0e086e510cd
Tony McCallie <tony@threeleaf.net>
2013-07-25 13:36:12 -0500

    Added referral form

--- app/Controller/ReferralsController.php
+++ app/Controller/ReferralsController.php
@@ -3,6 +3,17 @@ App::uses('AppController', 'Controller');
 class ReferralsController extends AppController {
 	public function confirm($shortcode = null) {
 		if(!empty($shortcode)) {
+			$referral = $this->Referral->findByShortcode($shortcode);
+			
+			if(!empty($this->request->data)) {
+				if($this->Referral->save($this->data)) {
+					$this->Session->setFlash('Thank you for your recommendation!','success');
+					$this->redirect('/');
+				}
+			} else {
+				$this->data = $referral;
+			}
+			$this->set(compact('referral'));
 			
 		} else {
 			$this->redirect('/');
--- app/View/Referrals/confirm.ctp
+++ app/View/Referrals/confirm.ctp
@@ -1,1 +1,11 @@
-REFERRAL
+<div class="span12">
+	<h2>Recommendation Form</h2>
+	<p>This is a recommendation form for <?php echo $referral['Application']['first_name'].' '.$referral['Application']['last_name'] ?></p>
+	<p>More interesting text about the importance of referrals...</p>
+	<?php
+		echo $this->Form->create();
+			echo $this->Form->input('id',array());
+			echo $this->Form->input('content',array());
+		echo $this->Form->end(array('label'=>'Save Recommendation','class'=>'btn'));
+	?>
+</div>
